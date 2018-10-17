e39e4936b2ba8ab0bff895f9f65e89ac382dae92
Kajsa Møllersen <kajsa.mollersen@uit.no>
2018-10-17 16:24:52 -0400

    Round 2
    Including a very important round 2, updating the h vectors.

--- binmatfac_lik.m
+++ binmatfac_lik.m
@@ -1,21 +1,33 @@
-function [W,H] = binmatfac_lik(X, Z, K)
+function [W,H] = binmatfac_lik(X, Z, K, fig_nr)
 
-% Requires:     select_column.m
+% Requires:     select_column_all.m
+
+% Before the probability parameters are estimated
 [n,d] = size(X);
 mask = false(n,d);
-% mask = logical(randi([0 1],n,d));
 
 W = false(n,K); 
 H = false(K,d);
 for k = 1: K
   tic
-  [w, h, Z] = select_column_likelihood(X,Z,mask);
+  [w, h, Z] = select_column_all(X,Z,mask);
   W(:,k) = w;
  
   H(k,:) = h;
   mask = mask | w*h;
   toc
-  k
+  figure(fig_nr), imagesc(mask), colormap(gray), title(k), drawnow
 end  
 
-
+Z = W;
+mask = false(n,d);
+for k = 1: K
+  tic
+  [w, h, Z] = select_column_all(X,Z,mask);
+  W(:,k) = w;
+ 
+  H(k,:) = h;
+  mask = mask | w*h;
+  toc
+  figure(fig_nr+1), imagesc(mask), colormap(gray), title(k), drawnow
+end  
