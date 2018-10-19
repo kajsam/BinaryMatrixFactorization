8cdf74c9a8093b13ba9788f58e4e12b60908dd2f
Kajsa Møllersen <kajsa.mollersen@uit.no>
2018-10-19 07:48:11 -0400

    Making a loop
    Cosmetic changes

--- EMasso_main.m
+++ EMasso_main.m
@@ -1,4 +1,4 @@
-function [A, Z, alphabeta_c, Pi] = EMasso_main(X, K, A0, Z0, fig_nr)
+function [A, Z] = EMasso_main(X, K, Z0, A0, fig_nr)
 
 % Requires:     passociation_matrix_otsu.m, binmatfac_lik.m, 
 %               select_column_likelihood.m 
@@ -8,61 +8,55 @@ function [A, Z, alphabeta_c, Pi] = EMasso_main(X, K, A0, Z0, fig_nr)
 %               A1 - Approximated structure matrix           
 
 [n,d] = size(X);
-
-
-%% First time is always special
-
-if A0 == 0
-  % Association matrix
-  'first go'
-  if Z0 == 0
-    tic
-    [Z0, tau] = passociation_matrix_otsu(X, X, 0, 0);
-    figure(fig_nr), imagesc(Z0), 
-    title(strcat('Median tau :', num2str(median(tau)))), colormap(gray)
-    toc
-  end
-  tic
-  [W,H] = binmatfac_lik(X, Z0, min(K,size(Z0,2)));
-  for k = 1: K
-    Ask = logical(W(:,k)*H(k,:));
-    figure(fig_nr+1), subplot(1,K,k)
-    imagesc(Ask), colormap(gray), xlabel(k), drawnow
-    if k == 1
-      ylabel('Components')
-      title('Initial A')
-    end
-  end
-  A0 = logical(W*H);
-  toc
-
-  figure(fig_nr+2), imagesc(A0), colormap(gray)  
-  title(strcat('Initial A, K = ',' ',num2str(K)))
-end
+n1 = sum(sum(X));
 
 A = cell(1,5);
 Z = cell(1,5);
 
-A{1} = A0;
-Z{1} = Z0;
-[alphabeta_c, cell_effect, Pi] = alpha_beta_cell(X, A{1}, fig_nr+3);
+%% First time is always special
 
-[Z{2}, thresh] = cell_association_matrix(X, A{1}, cell_effect,fig_nr+4);
-
-cZc = [Z{2} ~Z{2}];
-
-whos cZc
-[W,H] = binmatfac_cell_lik(X, cZc, min(K,size(Z{2},2)),alphabeta_c);
-for k = 1: K
-  Ask = logical(W(:,k)*H(k,:));
-  figure(fig_nr+5), subplot(1,K,k)
-  imagesc(Ask), colormap(gray), xlabel(k), drawnow
-  if k == 1
-    ylabel('Components')
-    title('First cell A')
-  end
+if Z0 == 0
+    tic
+  Z{1} = association_matrix_otsu(X, fig_nr);
+  toc
+  % figure(fig_nr), imagesc(Z{1})
+  title('Candidate columns'), colormap(gray)
+else
+  Z{1} = Z0;      
 end
-A{2} = logical(W*H);
 
-figure(fig_nr+6), imagesc(A{2}), colormap(gray)  
-title(strcat('First cell A, K = ',' ',num2str(K)))
+if A0 == 0
+  % Association matrix
+  tStart = tic;
+  [W,H] = binmatfac_lik(X, Z{1}, K, fig_nr+1);
+  tElapsed = toc(tStart)
+  A{1} = logical(W*H);
+else
+  A{1} = A0; 
+end
+figure(fig_nr), imagesc(Z{1}), 
+title('Candidate columns'), colormap(gray)
+figure(fig_nr+1), imagesc(A{1}), colormap(gray)  
+title(strcat('Initial A, K = ',' ',num2str(K)))
+[n1 sum(sum(A{1}))]
+% Complete rounds
+return
+for r = 1: 2
+  fig_nr = fig_nr+3;
+  [alphabeta_c, cell_effect] = alpha_beta_cell(X, A{r}, 0);
+
+  Z{r+1} = cell_association_matrix(X, A{r}, cell_effect,[fig_nr fig_nr+1]);
+  ylabel(r+1)
+  cc = size(Z{r+1},2);
+  K = min(K,floor(cc/2));
+  if K < 3
+    break
+  end
+
+
+  [W,H] = binmatfac_cell_lik(X, Z{r+1}, K,alphabeta_c, fig_nr+2);
+  A{r+1} = logical(W*H);
+  [n1 sum(sum(A{r+1}))]
+  title(strcat(num2str(r),'th round A, K = ',' ',num2str(min(K,size(Z{r+1},2)))))
+end
+
