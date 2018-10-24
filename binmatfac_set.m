181b0d6e4a7490f3090ccd51d1ae5f069275e8f8
Kajsa Møllersen <kajsa.mollersen@uit.no>
2018-10-24 10:46:59 -0400

    New turn
    This suddenly took a new turn. For the better, I hope.

--- binmatfac_set.m
+++ binmatfac_set.m
@@ -1,4 +1,4 @@
-function [W0,H0] = binmatfac_set(X, Z, Zsets,fig_nr)
+function [Zexp,H] = binmatfac_set(X, Z, Zsets,fig_nr)
 
 % Requires:     select_column_set.m
 
@@ -9,67 +9,115 @@ FPN = zeros(1, nZets);
 for set = 1: nZets
   Zet = Z(:,Zsets{set});
   K = size(Zet,2);
-  miss = n*d;
-  W = false(n,K); 
-  H = false(K,d);
-  mask = false(size(X));
-  tic
-  for  it = 1: 10
-    Zet = Z(:,Zsets{set});
-    for k = 1: K
-      [w, h, Zet] = select_column_set(X,Zet,mask);
-      W(:,k) = w;
-      H(k,:) = h;
-      mask = mask | w*h;
-    end  
-
-    FP = sum(sum(mask & ~X));
-    FN = sum(sum(~mask & X));
-   
-    if miss <= FP+FN
-      figure(fig_nr), imagesc(mask), colormap(gray), title([FP FN])
-      xlabel(set),  drawnow
-      break
-    else
-      miss = FP+FN;
-      FPN(set) = miss;
+  
+  Zexp = Zet;
+  % Expand the set to all cominations
+  for k = 2 : K-1
+    c = combnk(1:K,k);
+    for i = 1: size(c,1)
+      Zexp = [Zexp logical(sum(Zet(:,c(i,:)),2))];
     end
   end
+  Zexp = [Zexp logical(sum(Zet(:,1:K),2))];
+      
+  % figure(40), imagesc(Zexp), colormap(gray), 
+  
+  H = calculate_h(X,Zexp);
+  
+  A = logical(Zexp*H);
+  FP = sum(sum(A & ~X));
+  FN = sum(sum(~A & X));
+  figure(fig_nr), imagesc(A), colormap(gray), title([FP FN])
+  xlabel(set)
+  FPN(set) = FP+FN;
+    
+%   miss = n*d;
+%   W = false(n,K); 
+%   H = false(K,d);
+%   mask = false(size(X));
+%   tic
+%   for  it = 1: 10
+%     Zet = Z(:,Zsets{set});
+%     for k = 1: K
+%       [w, h, Zet] = select_column_set(X,Zet,mask);
+%       W(:,k) = w;
+%       H(k,:) = h;
+%       mask = mask | w*h;
+%     end  
+% 
+%     FP = sum(sum(mask & ~X));
+%     FN = sum(sum(~mask & X));
+%    
+%     if miss <= FP+FN
+%       figure(fig_nr), imagesc(mask), colormap(gray), title([FP FN])
+%       xlabel(set),  drawnow
+%       break
+%     else
+%       miss = FP+FN;
+%       FPN(set) = miss;
+%     end
+%   end
   toc
 end  
 
 [~,idx] = min(FPN)
 
-for set = idx
+ set = idx
   Zet = Z(:,Zsets{set});
   K = size(Zet,2)
-  miss = n*d;
-  W = false(n,K); 
-  H = false(K,d);
-  W0 = false(n,K); 
-  H0 = false(K,d);
-  mask = false(size(X));
-  
-  for  it = 1: 10
-    Zet = Z(:,Zsets{set});
-    for k = 1: K
-      [w, h, Zet] = select_column_set(X,Zet,mask);
-      W(:,k) = w;
-      H(k,:) = h;
-      mask = mask | w*h;
-    end  
-
-    FP = sum(sum(mask & ~X));
-    FN = sum(sum(~mask & X));
-    figure(fig_nr), imagesc(mask), colormap(gray), title([FP FN])
-    xlabel(idx),  drawnow
-    if miss <= FP+FN
-      break
-    else
-      miss = FP+FN;
-      W0 = W;
-      H0 = H;
-      FPN(set) = miss;
+  Zsets{set}
+  Zexp = Zet;
+  % Expand the set to all cominations
+  for k = 2 : K-1
+    c = combnk(1:K,k);
+    for i = 1: size(c,1)
+      Zexp = [Zexp logical(sum(Zet(:,c(i,:)),2))];
     end
   end
-end  
+  Zexp = [Zexp logical(sum(Zet(:,1:K),2))];
+      
+  figure(40), imagesc(Zet), colormap(gray), 
+  
+  H = calculate_h(X,Zexp);
+  
+  A = logical(Zexp*H);
+  FP = sum(sum(A & ~X));
+  FN = sum(sum(~A & X));
+  figure(fig_nr), imagesc(A), colormap(gray), title([FP FN])
+  xlabel(set)
+  FPN = FP+FN
+  pause
+
+% for set = idx
+%   Zet = Z(:,Zsets{set});
+%   K = size(Zet,2)
+%   miss = n*d;
+%   W = false(n,K); 
+%   H = false(K,d);
+%   W0 = false(n,K); 
+%   H0 = false(K,d);
+%   mask = false(size(X));
+%   
+%   for  it = 1: 10
+%     Zet = Z(:,Zsets{set});
+%     for k = 1: K
+%       [w, h, Zet] = select_column_set(X,Zet,mask);
+%       W(:,k) = w;
+%       H(k,:) = h;
+%       mask = mask | w*h;
+%     end  
+% 
+%     FP = sum(sum(mask & ~X));
+%     FN = sum(sum(~mask & X));
+%     figure(fig_nr), imagesc(mask), colormap(gray), title([FP FN])
+%     xlabel(idx),  drawnow
+%     if miss <= FP+FN
+%       break
+%     else
+%       miss = FP+FN;
+%       W0 = W;
+%       H0 = H;
+%       FPN(set) = miss;
+%     end
+%   end
+% end  
