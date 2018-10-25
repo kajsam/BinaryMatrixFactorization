85e07a132329db9940827de5ed5ed974ac0f48cc
Kajsa Møllersen <kajsa.mollersen@uit.no>
2018-10-25 11:08:57 -0400

    Quality of candidate columns
    Evaluate only the "best" 50% of the candidate columns

--- EMasso_sets.m
+++ EMasso_sets.m
@@ -1,4 +1,4 @@
-function [A, Z] = EMasso_sets(X, K, Z0, A0, fig_nr)
+function [A, Z, Zsets] = EMasso_sets(X, Z0, Zsets, A0, fig_nr)
 
 % Requires:     association_matrix_otsu.m, binmatfac_lik.m, 
 %               select_column_likelihood.m 
@@ -13,33 +13,55 @@ n1 = sum(sum(X));
 A = cell(1,5);
 Z = cell(1,5);
 
-min_class = ceil(0.01*n) % Minimum class size
+min_class = ceil(0.001*n) % Minimum class size
 max_class = floor(0.95*n) % Maximum class size
 
 %% First time is always special
 
 if Z0 == 0
     tic
-  Z{1} = association_matrix_otsu(X, min_class, fig_nr);
+  Z{1} = association_matrix_otsu(X, min_class, max_class, fig_nr);
   toc
   % figure(fig_nr), imagesc(Z{1})
   title('Candidate columns'), colormap(gray)
-  Z{1} = reduxZoverlap(Z{1}, min_class);
+  % Z{1} = reduxZoverlap(Z{1}, min_class);
 else
   Z{1} = Z0;      
 end
 drawnow
 
-
 figure(fig_nr+1), imagesc(Z{1}), colormap(gray), title(size(Z{1},2))
 drawnow
 
-Zsets = column_set(Z{1});
+sumZ = sum(Z{1},1);
+[~, idx] = sort(sumZ,'descend');
+Z{1} = Z{1}(:,idx);
+figure, imagesc(Z{1}), colormap(gray), title('Candidate columns sorted')
+
+if isempty(Zsets)
+    tic
+  Zsets = column_set(Z{1},0);
+  toc
+  
+  
+end
+
+qual = zeros(1, length(Zsets));
+  for set = 1: length(Zsets)
+    q = sum(Z{1}(:,Zsets{set}),2);
+    qual(set) = sum(~q)+ sum(q>1);
+  end
+  [~, qidx] = sort(qual,'ascend');
+Zsets_sort = Zsets(qidx);
+
+
+
 
 if A0 == 0
   % Association matrix
+  Zsets_half = Zsets_sort(1:floor(length(Zsets_sort)/2));
   tStart = tic;
-  [W,H] = binmatfac_set(X, Z{1}, Zsets, fig_nr+2);
+  [W,H] = binmatfac_set(X, Z{1}, Zsets_half, 0);
   tElapsed = toc(tStart)
   A{1} = logical(W*H);
 else
@@ -48,7 +70,7 @@ end
 figure(fig_nr), imagesc(Z{1}), 
 title('Candidate columns'), colormap(gray)
 figure(fig_nr+1), imagesc(A{1}), colormap(gray)  
-title(strcat('Initial A, K = ',' ',num2str(K)))
+title(strcat('Initial A, K = ',' ',num2str(size(W,2))))
 [n1 sum(sum(A{1}))]
 % Complete rounds
 return
